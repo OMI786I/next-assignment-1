0d5283745af7e3ad489fabdc2838531153b8d870
OMI786I <sohrawardy1998@gmail.com>
2025-05-07 22:56:46 +0600

    first solution

--- solutions.ts
+++ solutions.ts
@@ -1,1 +1,9 @@
-//testing
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === true || toUpper === undefined) {
+    return input.toLocaleUpperCase();
+  } else if (toUpper === false) {
+    return input.toLocaleLowerCase();
+  }
+
+  return input;
+}
